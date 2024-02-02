b76e2ba2e2cc386c6ab728b84c1141dc4a8c18e1
Jenny Arias <jennyarias@Jennys-Air.attlocal.net>
2024-02-02 13:03:31 -0600

    initial commit

---
+++ src/components/homeComponents/HomeScreen.jsx
@@ -0,0 +1,29 @@
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
+import AdBanner from './AdBanner'
+
+const HomeScreen = () => {  
+  const [recipes, setRecipes] = useState('');
+
+  const getRecipes = () => {
+    axios
+        .get("https://recipes.devmountain.com/recipes")
+        .then((res) => {
+            setRecipes(res.data)
+            console.log(res.data)
+        })
+}
+
+useEffect(() => {
+    getRecipes()
+},[])
+
+  return (
+    <div>
+      <AdBanner />
+      {/* Much code from Part 2 will be placed around here. Do your best! */}
+    </div>
+  )
+}
+
+export default HomeScreen
