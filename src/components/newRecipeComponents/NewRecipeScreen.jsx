c7880d2ce55643ed1809df1c80541071fd9ef8fa
Jenny Arias <jennyarias@Jennys-Air.attlocal.net>
2024-02-04 21:43:31 -0600

    part 4 complete

--- src/components/newRecipeComponents/NewRecipeScreen.jsx
+++ src/components/newRecipeComponents/NewRecipeScreen.jsx
@@ -1,150 +1,186 @@
 import React, { useState } from 'react';
 import styles from './NewRecipe.module.css';
+import { Formik } from "formik";
 
 
 
 const NewRecipeScreen = () => {
-  const [formData, setFormData] = useState({
-    recipeName: '',
-    imageURL: '',
-    prepTime: '',
-    cookTime: '',
-    serves: '',
-    ingredient: '',
-    quantity: '',
-    instructions: '',
-    cookingMethod: '',
-  });
+  const [ingredients, setIngredients] = useState([]);
+  const [name, setName] = useState("");
+  const [quantity, setQuantity] = useState("");
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
+  const initialValues = {
+    type: "",
+    recipeName: "",
+    imageURL: "",
+    prepTime: "",
+    cookTime: "",
+    serves: "",
+    ingredients: [],
+    instructions: "",
   };
 
   const handleIngredientAdd = () => {
-    if (formData.ingredient && formData.quantity) {
-      // Assuming ingredients are stored as an array of objects
-      setFormData({
-        ...formData,
-        ingredients: [
-          ...(formData.ingredients || []),
-          {
-            ingredient: formData.ingredient,
-            quantity: formData.quantity,
-          },
-        ],
-        ingredient: '',
-        quantity: '',
-      });
-    }
+    setIngredients([...ingredients, { name, quantity }]);
+    setName("");
+    setQuantity("");
   };
+
+  const onSubmit = (values) => {
+    values.ingredients = ingredients;
+    console.log(values);
+  };
+
+  // const [formData, setFormData] = useState({
+  //   recipeName: "",
+  //   imageURL: "",
+  //   prepTime: "",
+  //   cookTime: "",
+  //   serves: "",
+  //   ingredient: "",
+  //   quantity: "",
+  //   instructions: "",
+  //   cookingMethod: "",
+  // });
+
+  // const handleChange = (e) => {
+  //   const { name, value } = e.target;
+  //   setFormData({ ...formData, [name]: value });
+  // };
+
+  // const handleIngredientAdd = () => {
+  //   if (formData.ingredient && formData.quantity) {
+  //     // Assuming ingredients are stored as an array of objects
+  //     setFormData({
+  //       ...formData,
+  //       ingredients: [
+  //         ...(formData.ingredients || []),
+  //         {
+  //           ingredient: formData.ingredient,
+  //           quantity: formData.quantity,
+  //         },
+  //       ],
+  //       ingredient: "",
+  //       quantity: "",
+  //     });
+  //   }
+  // };
 
   return (
     <section>
       <h1>Tell us about your Recipe!</h1>
       {/* Here you will have a large form. Be prepared, part 4 will have you build this form in detail, and part 5 will have you style it. Do your best! */}
-      <form>
-        <input
-          type="text"
-          placeholder="Recipe Name"
-          name="recipeName"
-          value={formData.recipeName}
-          onChange={handleChange}
-        />
+      <Formik initialValues={initialValues} onSubmit={onSubmit}>
+        {({ values, handleChange, handleSubmit }) => (
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              placeholder="Recipe Name"
+              name="recipeName"
+              value={values.recipeName} //formData.recipeName
+              onChange={handleChange}
+            />
 
-        <input
-          type="text"
-          placeholder="Image URL"
-          name="imageURL"
-          value={formData.imageURL}
-          onChange={handleChange}
-        />
+            <input
+              type="text"
+              placeholder="Image URL"
+              name="imageURL"
+              value={values.imageURL}
+              onChange={handleChange}
+            />
 
-        <label>
-          Cook
-          <input
-            type="radio"
-            name="cookingMethod"
-            value="Cook"
-            checked={formData.cookingMethod === "Cook"}
-            onChange={handleChange}
-          />
-        </label>
-        <label>
-          Bake
-          <input
-            type="radio"
-            name="cookingMethod"
-            value="Bake"
-            checked={formData.cookingMethod === "Bake"}
-            onChange={handleChange}
-          />
-        </label>
-        <label>
-          Drink
-          <input
-            type="radio"
-            name="cookingMethod"
-            value="Drink"
-            checked={formData.cookingMethod === "Drink"}
-            onChange={handleChange}
-          />
-        </label>
+            <label>
+              Cook
+              <input
+                type="radio"
+                name="cookingMethod"
+                value="Cook"
+                // checked={formData.cookingMethod === "Cook"}
+                onChange={handleChange}
+              />
+            </label>
+            <label>
+              Bake
+              <input
+                type="radio"
+                name="cookingMethod"
+                value="Bake"
+                // checked={formData.cookingMethod === "Bake"}
+                onChange={handleChange}
+              />
+            </label>
+            <label>
+              Drink
+              <input
+                type="radio"
+                value="Drink"
+                onChange={handleChange}
+                name="cookingMethod"
 
-        <input
-          type="text"
-          placeholder="Prep Time"
-          name="prepTime"
-          value={formData.prepTime}
-          onChange={handleChange}
-        />
+                // checked={formData.cookingMethod === "Drink"}
+              />
+            </label>
 
-        <input
-          type="text"
-          placeholder="Cook Time"
-          name="cookTime"
-          value={formData.cookTime}
-          onChange={handleChange}
-        />
+            <input
+              type="text"
+              placeholder="Prep Time"
+              name="prepTime"
+              value={values.prepTime}
+              onChange={handleChange}
+            />
 
-        <input
-          type="text"
-          placeholder="Serves"
-          name="serves"
-          value={formData.serves}
-          onChange={handleChange}
-        />
+            <input
+              type="text"
+              placeholder="Cook Time"
+              name="cookTime"
+              value={values.cookTime}
+              onChange={handleChange}
+            />
 
-        <input
-          type="text"
-          placeholder="Ingredient"
-          name="ingredient"
-          value={formData.ingredient}
-          onChange={handleChange}
-        />
+            <input
+              type="text"
+              placeholder="Serves"
+              name="serves"
+              value={values.serves}
+              onChange={handleChange}
+            />
 
-        <input
-          type="text"
-          placeholder="Quantity"
-          name="quantity"
-          value={formData.quantity}
-          onChange={handleChange}
-        />
+            <input
+              type="text"
+              placeholder="Ingredient"
+              name="ingredient"
+              // value={formData.ingredient}
+              // onChange={handleChange}
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
 
-        <button type="button" onClick={handleIngredientAdd}>
-          Add Another
-        </button>
+            <input
+              type="text"
+              placeholder="Quantity"
+              name="quantity"
+              // value={formData.quantity}
+              // onChange={handleChange}
+              value={quantity}
+              onChange={(e) => setQuantity(e.target.value)}
+            />
 
-        <textarea
-          name="instructions"
-          placeholder="What are the instructions?"
-          value={formData.instructions}
-          onChange={handleChange}
-        ></textarea>
+            <button type="button" onClick={handleIngredientAdd}>
+              Add Another
+            </button>
 
-        {/* Add submit button here */}
-        <button type="submit">Save</button>
-      </form>
+            <textarea
+              name="instructions"
+              placeholder="What are the instructions?"
+              value={values.instructions}
+              onChange={handleChange}
+            ></textarea>
+
+            {/* Add submit button here */}
+            <button type="submit">Save</button>
+          </form>
+        )}
+      </Formik>
     </section>
   );
 };
